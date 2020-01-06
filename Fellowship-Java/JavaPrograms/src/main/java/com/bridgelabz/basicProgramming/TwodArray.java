e6cf00aa3e39042618982bc0733f0f83f2a8b849
Mukesh0086 <mukeshgsmukesh@gmail.com>
2020-01-06 17:25:42 +0530

    [Mukesh] Adding twodArray.java file

--- Fellowship-Java/JavaPrograms/src/main/java/com/bridgelabz/basicProgramming/TwodArray.java
+++ Fellowship-Java/JavaPrograms/src/main/java/com/bridgelabz/basicProgramming/TwodArray.java
@@ -5,28 +5,34 @@ import java.io.PrintWriter;
 import com.bridgelabz.utility.Utility;
 
 public class TwodArray {
+	
 	public static void main(String[] args) {
+		
 		System.out.println("Enter Rows");
 		int rows=Utility.readInteger();
 		System.out.println("Enter Columns");
 		int columns=Utility.readInteger();
-		int addArray[][]=twodArrayInputs(2, 2);
-		int printArray[][]=print2dArray(addArray, 2, 2);
+		int addArray[][]=twodArrayInputs(rows, columns);
+		int printArray[][]=print2dArray(addArray, rows, columns);
+		
+		
 	}
 	public static int [][] twodArrayInputs(int rows ,int columns)
 	{
-		System.out.println("inside inputs");
+		
 		int [][] twodArray=new int[rows][columns];
 		for(int i=0;i<=rows;i++)
 		{
 			for(int j=0;j<=columns;j++)
 			{
+				System.out.print("Enter Array Values");
 				int input=Utility.readInteger();
-				System.out.println("Enter Array Values");
+				
 				
 				twodArray[i][j]=input;
 				
 			}
+			System.out.println("");
 		}
 		
 		
@@ -38,12 +44,13 @@ public class TwodArray {
 	public static int [][] print2dArray(int [][]twodArray,int rows,int columns)
 	{
 		PrintWriter pw=new PrintWriter(System.out,true);
-		for(int i=0;i<rows;i++)
+		for(int i=0;i<=rows;i++)
 		{
-			for(int j=0;j<columns;j++)
+			for(int j=0;j<=columns;j++)
 			{
 				System.out.println((twodArray[i][j]));
 			}
+			System.out.println("");
 		}
 		return twodArray;
 		
